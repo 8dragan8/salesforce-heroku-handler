88cc2834074ab5881f5d07e171b288337384b196
8dragan8 <35200825+8dragan8@users.noreply.github.com>
2021-02-18 20:54:51 +0100

    inplemented salesforce error

--- web/index.php
+++ web/index.php
@@ -1,51 +1,70 @@
 <?php
-header("Content-Type: application/json; charset=UTF-8");
 
-$url = 'https://app1.renderator.com/php/api/inboundHanlder.php?token=1609941097';
+$secure = true;
+require_once('libs/SalesforceInboundHandler.php');
 
-$xml = file_get_contents('php://input');
 
-$response = false;
-if ($xml) {
 
-    //Initiate cURL
-    $curl = curl_init($url);
 
-    //Set the Content-Type to text/xml.
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
 
-    //Set CURLOPT_POST to true to send a POST request.
-    curl_setopt($curl, CURLOPT_POST, true);
+$handler = new SalesforceInboundHandler(false); // default = false , true means will use sample instead posted data.
+$handler->SalesforceToMysqlSyncWithApi();
 
-    //Attach the XML string to the body of our request.
-    curl_setopt($curl, CURLOPT_POSTFIELDS, $xml);
 
-    //Tell cURL that we want the response to be returned as
-    //a string instead of being dumped to the output.
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$fields = array("unit__c", "status");
 
-    //Execute the POST request and send our XML.
-    $result = curl_exec($curl);
+if ($handler->isError == false && $handler->records) {
 
-    //Do some basic error checking.
-    if (curl_errno($curl)) {
-        $err = new Exception(curl_error($curl));
-        $response = [
-            "xml" => $xml,
-            "result" => $result,
-            "err" => $err
-        ];
-        throw $err;
+    $json  = $handler->getJsonWithFields($fields);
+    //some process here.
+
+    error_log($json);
+
+    $url = 'https://app1.renderator.com/php/api/inboundHanlder.php?token=1609941097';
+
+    $xml = file_get_contents('php://input');
+
+    $response = false;
+    if ($xml) {
+
+        //Initiate cURL
+        $curl = curl_init($url);
+
+        //Set the Content-Type to text/xml.
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
+
+        //Set CURLOPT_POST to true to send a POST request.
+        curl_setopt($curl, CURLOPT_POST, true);
+
+        //Attach the XML string to the body of our request.
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $xml);
+
+        //Tell cURL that we want the response to be returned as
+        //a string instead of being dumped to the output.
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        //Execute the POST request and send our XML.
+        $result = curl_exec($curl);
+
+        //Do some basic error checking.
+        if (curl_errno($curl)) {
+            $err = new Exception(curl_error($curl));
+            $response = [
+                "xml" => $xml,
+                "result" => $result,
+                "err" => $err
+            ];
+            throw $err;
+        }
+
+        //Close the cURL handle.
+        curl_close($curl);
     }
 
-    //Close the cURL handle.
-    curl_close($curl);
 
-    //Print out the response output.
-    $response = [
-        "xml" => $xml,
-        "result" => $result,
-    ];
+    $handler->respondSuccess();
+} else {
+
+    $handler->respondBad($handler->errorMsg);
 }
-
-echo json_encode($response);
+// header("Content-Type: application/json; charset=UTF-8");
